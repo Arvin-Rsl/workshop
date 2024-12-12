fe412dcc6a9139a469f923b8f61df08c6e75b54e
Arvin-Rsl <arvinrasulzadeh@gmail.com>
2024-12-12 13:22:42 +0330

    Arvin updated hello.c
    Adding the age of the universe!!

--- hello.c
+++ hello.c
@@ -2,6 +2,10 @@
 int main()
 {
 	printf("Hello, World!\n");
+
+	// I'm Arvin, and this is my edit on "hello.c"
+	printf("Did you know the age of our universe is about 13.7 Gyr?!");
+	
 	return 0;
 }
 
